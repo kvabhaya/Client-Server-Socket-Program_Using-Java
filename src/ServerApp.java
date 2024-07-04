f5fc2c34fb55e01559c7ef60670f37ca8e434f70
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-04 21:34:56 +0530

    complete serverApp 1.0

--- src/ServerApp.java
+++ src/ServerApp.java
@@ -1,24 +1,17 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ServerApp {
+    private static Set<PrintWriter> clientWriters = new HashSet<>();
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(8082);
-            System.out.println("Server is up & running. now waiting for a client...");
-
-            Socket socket = serverSocket.accept();
-            System.out.println("client connected...");
-
-            OutputStream outputStream = socket.getOutputStream();
-            PrintWriter printWriter = new PrintWriter(outputStream, true);
-
-            printWriter.println("Hello client...");
-
-            socket.close();
-            serverSocket.close();
-
+        System.out.println("Started...");
+        try(ServerSocket serverSocket = new ServerSocket(8082)){
+            while (true){
+                new Handler(serverSocket.accept()).start();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,14 +32,14 @@ public class ServerApp {
             try {
                 bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 printWriter = new PrintWriter(socket.getOutputStream(), true);
-                synchronized (null) {
-
+                synchronized (clientWriters) {
+                    clientWriters.add(printWriter);
                 }
                 String message;
                 while ((message = bufferedReader.readLine()) != null) {
                     System.out.println("Received : " + message);
-                    synchronized (null) {
-                        for (PrintWriter writer : null) {
+                    synchronized (clientWriters) {
+                        for (PrintWriter writer : clientWriters) {
                             writer.write(message);
                         }
                     }
@@ -59,8 +52,8 @@ public class ServerApp {
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
-                synchronized (null) {
-
+                synchronized (clientWriters) {
+                    clientWriters.remove(printWriter);
                 }
             }
         }
